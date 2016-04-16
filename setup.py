9733bb3654b8eb660850e1b5975769e0c4acd408
Yifei Zhang <njzhangyifei@gmail.com>
2016-04-16 05:53:45 -0700

    setup script

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
 from distutils.core import setup
 
 setup(
     name='pyDexLink',
     version='0.0.1',
     packages=['dexlink', 'dexlink.util'],
-    url='',
+    url='https://github.com/njzhangyifei/pyDexLink',
     license='MIT License',
     author='Yifei Zhang',
     author_email='njzhangyifei@gmail.com',
