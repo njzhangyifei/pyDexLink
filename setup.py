0213f7ecd9cfa1e17c19c99767e987fc6017346a
Yifei Zhang <njzhangyifei@gmail.com>
2016-04-16 05:50:59 -0700

    added setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyDexLink',
+    version='0.0.1',
+    packages=['dexlink', 'dexlink.util'],
+    url='',
+    license='MIT License',
+    author='Yifei Zhang',
+    author_email='njzhangyifei@gmail.com',
+    description='Python implementation of the DexLink protocol'
+)
